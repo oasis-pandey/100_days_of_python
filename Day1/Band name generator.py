7bd0aeab31e43804a5447d97bb4a232f88c99160
oasis_pandey <oasispandey2000@gmail.com>
2025-01-23 21:02:52 -0500

    randoms, lists

--- Day1/Band name generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.\n")
-city=input("Whats the city you grew up in? \n")
-pet=input("Whats the name of your pet?\n ")
-print("Your band name could be "+ (city+" " +pet +"."))
